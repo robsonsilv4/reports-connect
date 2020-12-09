89cd148dcdc37e98bc28b58538247c267d217cfb
catskillet.rsilva <catskillet.rsilva@hubfintech.com.br>
2020-12-09 17:15:31 -0300

    fix: add str in report models

--- reports/models.py
+++ reports/models.py
@@ -11,8 +11,14 @@ class Report(models.Model):
     )
     supervisors = models.ManyToManyField(settings.AUTH_USER_MODEL)
 
+    def __str__(self):
+        return f"Report {self.id} - {self.message}"
+
 
 class ReportResponse(models.Model):
     message = models.TextField(max_length=255)
     report = models.ForeignKey(Report, on_delete=models.CASCADE)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Report {self.report.id} - {self.message}"
