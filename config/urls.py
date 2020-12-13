eb52b3de2d2e0b8d0852036ada5a2c8efe14f14d
catskillet.rsilva <catskillet.rsilva@hubfintech.com.br>
2020-12-13 09:28:07 -0300

    feat: add whitenoise in development

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
@@ -8,7 +6,4 @@ urlpatterns = [
     path("", include("reports.urls")),
     path("users/", include("users.urls")),
     path("admin/", admin.site.urls),
-] + static(
-    settings.STATIC_URL,
-    document_root=settings.STATIC_ROOT,
-)
+]
